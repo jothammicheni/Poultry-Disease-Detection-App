9f03d46ba3699da68b3c5dd74bfe5166b619c243
jothammicheni <jothammurimi2001@gmail.com>
2024-05-25 21:28:01 +0300

    new

---
+++ app/src/main/java/com/example/layersdiseasedetection/ChatActivity.java
@@ -0,0 +1,27 @@
+package com.example.layersdiseasedetection;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.layersdiseasedetection.databinding.ActivityChatBinding;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+
+    }
+}
