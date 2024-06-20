d3edc353f3dd844015103311ff19f8f3d7b60928
jothammicheni <jothammurimi2001@gmail.com>
2024-06-20 16:51:40 +0300

    new

--- app/src/main/java/com/example/layersdiseasedetection/DiseaseSpread.java
+++ app/src/main/java/com/example/layersdiseasedetection/DiseaseSpread.java
@@ -2,21 +2,26 @@ package com.example.layersdiseasedetection;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class DiseaseSpread extends AppCompatActivity {
+
     private EditText etFlockNumber, etInitialInfected, etSimulationDays;
     private Button btnSimulate;
-    private TextView tvFinalInfected, tvFinalRecovered;
+    private TextView tvFinalInfected, tvFinalRecovered, tvFinalDead;
+    private Spinner spVaccination;
+    private double vaccinationRate = 0.0;
+    private double deathRate  = 0.001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,31 @@ public class DiseaseSpread extends AppCompatActivity {
         btnSimulate = findViewById(R.id.btnSimulate);
         tvFinalInfected = findViewById(R.id.tvFinalInfected);
         tvFinalRecovered = findViewById(R.id.tvFinalRecovered);
+        tvFinalDead = findViewById(R.id.tvFinalDead);
+        spVaccination = findViewById(R.id.spinnerVaccination);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.vaccination_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spVaccination.setAdapter(adapter);
+        spVaccination.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selected = parentView.getItemAtPosition(position).toString();
+                if (selected.equals("Yes")) {
+                    vaccinationRate = 1.0;
+                    deathRate=0.00;
+                } else {
+                    vaccinationRate = 0.0;
+                    deathRate=0.09;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                vaccinationRate = 0.0;
+            }
+        });
 
         btnSimulate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,13 +70,13 @@ public class DiseaseSpread extends AppCompatActivity {
 
                     double spreadRate = 0.3;
                     double recoveryRate = 0.1;
-                    double deathRate = 0.09;
-                    double vaccinationRate = 0.5;
+
 
                     SimulationResult result = simulateDiseaseSpread(numBirds, initialInfected, spreadRate, recoveryRate, vaccinationRate, days, deathRate);
 
-                    tvFinalInfected.setText(""+result.finalInfected);
-                    tvFinalRecovered.setText(""+result.finalRecovered);
+                    tvFinalInfected.setText(String.valueOf((int) result.finalInfected));
+                    tvFinalRecovered.setText(String.valueOf((int) result.finalRecovered));
+                    tvFinalDead.setText(String.valueOf((int) result.finalDead));
 
                     Toast.makeText(DiseaseSpread.this, "Simulation Complete", Toast.LENGTH_SHORT).show();
                 } catch (NumberFormatException e) {
@@ -61,34 +91,36 @@ public class DiseaseSpread extends AppCompatActivity {
         double finalRecovered;
         double finalDead;
 
-        public SimulationResult(double finalInfected, double finalRecovered,double finalDead) {
+        public SimulationResult(double finalInfected, double finalRecovered, double finalDead) {
             this.finalInfected = finalInfected;
             this.finalRecovered = finalRecovered;
-            this.finalDead=finalDead;
+            this.finalDead = finalDead;
         }
     }
 
-    public static SimulationResult simulateDiseaseSpread(int numBirds, int initialInfected, double spreadRate, double recoveryRate, double vaccinationRate, int days,double  deathRate) {
+    public static SimulationResult simulateDiseaseSpread(int numBirds, int initialInfected, double spreadRate, double recoveryRate, double vaccinationRate, int days, double deathRate) {
         double susceptible = numBirds - initialInfected;
         double infected = initialInfected;
         double recovered = 0;
-        double  dead=0;
+        double dead = 0;
 
         // Apply vaccination
         double vaccinated = vaccinationRate * susceptible;
-         susceptible -= vaccinated;
+        susceptible -= vaccinated;
 
         for (int day = 0; day < days; day++) {
-            // Calculate the number of new infections and recoveries
+            // Calculate the number of new infections, recoveries, and deaths
             double newInfected = spreadRate * susceptible * infected / numBirds;
             double newRecovered = recoveryRate * infected;
+            double newDeaths = deathRate * infected;
 
             // Update the states
             susceptible -= newInfected;
-            infected += newInfected - newRecovered;
+            infected += newInfected - newRecovered - newDeaths;
             recovered += newRecovered;
+            dead += newDeaths;
         }
 
-        return new SimulationResult(infected, recovered,dead);
+        return new SimulationResult(infected, recovered, dead);
     }
-    }
+}
